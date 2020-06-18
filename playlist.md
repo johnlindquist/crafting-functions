a5e8dd13e84fcdd5ace34bb6bfa4a582b61bb6dc
johnlindquist <johnlindquist@gmail.com>
2020-06-18 17:21:58 -0600

    Thu Jun 18 17:21:58 MDT 2020

---
+++ playlist.md
@@ -0,0 +1,7 @@
+
+### Wrap-Settimeout 
+
+[Go to branch](https://github.com/johnlindquist/crafting-functions/tree/wrap-settimeout) 
+
+[Edit on Codesandbox](https://codesandbox.io/s/github/johnlindquist/crafting-functions/tree/wrap-settimeout/?module=/src/index.js) 
+
--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
-import "./styles.css";
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use Parcel to bundle this sandbox, you can find more info about Parcel
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+
+let createTimeout = (time) => (callback) => {
+  setTimeout(callback, time)
+}
+
+let oneSecond = createTimeout(1000)
+let twoSeconds = createTimeout(2000)
+let threeSeconds = createTimeout(3000)
+
+oneSecond(() => {
+  console.log("one")
+})
+twoSeconds(() => {
+  console.log("two")
+})
+threeSeconds(() => {
+  console.log("three")
+})
